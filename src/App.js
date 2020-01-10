911d498ab3f282fba091f1104ae6bc9d547eaf8a
João Lima <jpalvesl101@gmail.com>
2020-01-10 14:58:39 -0300

    Adicionando seção de informações acadêmicas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import About from './pages/About/index';
 import Personal from './pages/Personal/index';
+import Academic from './pages/Academic/index';
 
 function App() {
   return (
     <div>
       <About />
       <Personal />
+      <Academic />
     </div>
     
   );
---
+++ src/pages/Academic/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import infos from '../../assets/data/academic.json';
+
+
+function Professional() {
+  return (
+    <section id="academic">
+      <h3>Informações acadêmicas</h3>
+      <h4>Formação</h4>
+      <ul id="formation">
+  {infos.formations.map((item) => (<li>({item.entry}-{item.depeature}) <br/> {item.course} {item.place} </li>))}
+      </ul>
+
+      <h4>Projetos</h4>
+      <ul id="projects">
+  {infos.projects.map((item) => (<li>({item.year}) <br/> {item.name} - {item.type}</li>))}
+      </ul>
+    </section>
+  );
+}
+
+export default Professional;
